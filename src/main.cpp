5dc32cd091889b74e8ea257e685b44ffccaf6c16
yanjie.sang <yanjie.sang@yubot.com>
2020-12-30 18:20:13 +0800

    增加共享内存处理线程

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@
 #include "Common.hpp"
 #include "mmap.hpp"
 
+#define SHM_SIZE                        200*1024
 #define MMAP_SIZE                       2*1024*1024
 #define NUMBER_OF_H264_FRAME_FILES      403
 #define DEFAULT_FPS_VALUE               25
@@ -29,6 +30,8 @@
 bool LogOutEnable=false;
 
 std::unique_ptr<Posix::Mmap<char>> MmapObj=nullptr;
+SystemV::Sem *MampSemObject=nullptr;
+SystemV::Shm *ShmObject=nullptr;
 SystemV::Sem *ShmSemObject=nullptr;
 int thread_running=0;
 
@@ -56,7 +59,7 @@ void *WriteMmapThreadRoutine(void *arg)
 
     while(thread_running)
     {
-        ShmSemObject->Decrease(0);
+        MampSemObject->Decrease(0);
         if(MmapObj!=nullptr)
         {
             bzero(filename,sizeof(filename)/sizeof(char));
@@ -76,11 +79,36 @@ void *WriteMmapThreadRoutine(void *arg)
             }
         }
         usleep(interval_time);
-        ShmSemObject->Increase(1); 
+        MampSemObject->Increase(1); 
     }
     LOG_OUT(INFO,"WriteMmapThreadRoutine Exit Normally");
     pthread_exit(NULL);
 } 
+
+void *WriteShmThreadRoutine(void *arg)
+{
+    int count=0;
+    char writebuffer[SHM_SIZE];
+    while(thread_running)
+    {
+        bzero(writebuffer,sizeof(writebuffer)/sizeof(char));
+
+        // Utils::JsonParser::JsonDocument JsonDoc;
+        // JsonDoc.SetObject();
+        // JsonDoc.AddMember("Count",count++,JsonDoc.GetAllocator());
+        // memcpy(writebuffer,
+        //         Utils::JsonParser::ToString(JsonDoc).c_str(),
+        //         Utils::JsonParser::ToString(JsonDoc).size());
+
+        ShmSemObject->Decrease(0);   // P操作锁定临界资源
+        // ShmObject->Send(writebuffer,0,SHM_SIZE);
+        ShmSemObject->Increase(1);   // V操作解锁临界资源
+
+        // LOG_OUT(INFO,"Shm Send Data:\n%s\n",Utils::JsonParser::ToString(JsonDoc).c_str());
+        //每隔2秒向共享内存写入数据
+        sleep(2);
+    }
+}
 
 void sigcatch(int signo)
 {
@@ -110,20 +138,28 @@ int main(int argc,char *argv[])
         }
     }
 
+    ShmObject=new SystemV::Shm("/",3,SHM_SIZE);
+    int shmsemval[2] = {1 , 0};
+    ShmSemObject=new SystemV::Sem("/",4,shmsemval,sizeof(shmsemval)/sizeof(int));
+
     MmapObj=Posix::Mmap<char>::Creat("/tmp/YgAppMmapFile",MMAP_SIZE,PROT_READ|PROT_WRITE,MAP_SHARED);
     if(MmapObj==nullptr)
     {
         LOG_OUT(ERROR,"mmap Creat Error!\n");
         exit(EXIT_FAILURE);
     }
-    LOG_OUT(INFO,"Frame Send Process Runing...\n");
+    LOG_OUT(INFO,"CubeCode Process Runing...\n");
 
-    int semval[2] = {1 , 0};//创建两个信号量用来共享内存互斥读写
-    ShmSemObject=new SystemV::Sem("/",5,semval,sizeof(semval)/sizeof(int));// 两个不同的进程进行信号量通信，需要保持一样的信号量值以及ipc_key
+    int mampsemval[2] = {1 , 0};//创建两个信号量用来共享内存互斥读写
+    MampSemObject=new SystemV::Sem("/",5,mampsemval,sizeof(mampsemval)/sizeof(int));
 
     thread_running=1;
     pthread_create(&MmapTid,NULL,WriteMmapThreadRoutine,NULL);
     pthread_detach(MmapTid);
+
+    pthread_t WriteShmTid;
+    pthread_create(&WriteShmTid,NULL,WriteShmThreadRoutine,NULL);
+    pthread_detach(WriteShmTid);
 
     while(1)
     {
